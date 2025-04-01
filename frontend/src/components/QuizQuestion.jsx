617e0d60758b8e216ba6a8adb072920e723ba7ec
Chanuka Isuru <139777352+Chanukaisuru@users.noreply.github.com>
2025-04-01 19:11:10 +0530

    create QuizQuestion

---
+++ frontend/src/components/QuizQuestion.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+
+const QuizQuestion =({
+    question,
+    options,
+    onAnswerSelect,
+    currentQuestionIndex,
+    totalQuestions,
+}) => {
+    return(
+        <div className='quiz-question-container'>
+            <div className='question-header'>
+                <h2>Question {currentQuestionIndex + 1} of {totalquestion}</h2>
+            </div>
+            <div className='question-text'>
+                <p>{question}</p>
+            </div>
+            <div className='answer-options'>
+                {options.map((option, index) => (
+                  <button
+                    key ={index}
+                    onClick={() => onAnswerSelect(option.points)}
+                    className='option-button'
+                 >  
+                    {option.text}
+                  </button>
+                ))}
+            </div>    
+        </div>
+    );
+};
+
+export default QuizQuestion;
