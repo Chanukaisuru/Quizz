423dda1cf883e46fa4f19f66256a1ecf4455b7ad
Chanuka Isuru <139777352+Chanukaisuru@users.noreply.github.com>
2025-04-01 20:08:24 +0530

    add other pages to app js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import QuizQuestion from './components/QuizQuestion';
+import { Questions } from './data/Questions';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  
+  
+  const handleAnswerSelect = (points) => {
+    //add points to score
+    setSelectionRange(prevScore => prevScore + points);
+    
+    // Move to the next question
+    if (currentQuestionIndex < Questions.length - 1) {
+      setCurrentQuestionIndex(prevIndex => prevIndex + 1);
+    }else{
+      // Quiz completed
+      setQuizCompleted(true);
+    }
+  };
+}  
 export default App;
