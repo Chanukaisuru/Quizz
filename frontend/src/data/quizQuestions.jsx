e4a4868cf64299a8a7d53de13b2d01842538ee53
Chanuka Isuru <139777352+Chanukaisuru@users.noreply.github.com>
2025-04-01 18:23:48 +0530

    create quizQuestion.jsx

---
+++ frontend/src/data/quizQuestions.jsx
@@ -0,0 +1,47 @@
+export const quizQuestions = [
+    {
+      id: 11,
+      question: "What is the correct order of structure in Google Ads campaigns?",
+      options: [
+        { 
+          text: "1. Campaigns, 2. Adsets, 3. Ads", 
+          points: 0 
+        },
+        { 
+          text: "1. Campaigns, 2. Ad Groups, 3. Ad Sets, 4. Ads", 
+          points: 0 
+        },
+        { 
+          text: "1. Campaigns, 2. Ad Groups, 3. Ads", 
+          points: 5 
+        },
+        { 
+          text: "1. Ad Sets, 2. Ad Groups, 3. Ads", 
+          points: 0 
+        }
+      ]
+    },
+    {
+      id: 12,
+      question: "What is the latest character limit for Google Text Ad headlines?",
+      options: [
+        { 
+          text: "One headline of 25 characters", 
+          points: 0 
+        },
+        { 
+          text: "One headline of 90 characters", 
+          points: 0 
+        },
+        { 
+          text: "Two headlines of 45 characters", 
+          points: 0 
+        },
+        { 
+          text: "Three headlines of 30 characters each", 
+          points: 5 
+        }
+      ]
+    },
+    // more questions can be added here
+  ];
